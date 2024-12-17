ef90e729898ac4974169372b8754cd8f4e57c710
yooud <khijnyak.dima@gmail.com>
2024-12-17 15:51:37 +0200

    configure naming policy

--- Airsense.API/Program.cs
+++ Airsense.API/Program.cs
@@ -1,4 +1,16 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateSlimBuilder(args);
+
+builder.Services.AddMvc()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower;
+        options.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.SnakeCaseLower;
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.SnakeCaseLower));
+    });
 
 var app = builder.Build();
 
